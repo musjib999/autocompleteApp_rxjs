2378f4a43047d264a532f99fb93e9b627431a13c
Musa Jibril <musjib999@gmail.com>
2020-09-20 17:25:19 +0100

    Changes

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+let store = ['Musa', 'Mubarak', 'Usman', 'Umar', 'Omale', 'Ayuba', 'Shams', 'Kiki'];
+
+app.get('/user', (req, res) => {
+    const {search} = req.query;
+    const result = doSearch(search);
+    res.status(200).json({ status: 'success', payload: result, message: 'Fetched successfully' });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(9000, () => {
+    console.log('Server listening on port 9000');
+});
+
+function doSearch(searchTerm) {
+   let temp = [];
+   store.forEach(obj => {
+       if(obj.startsWith(searchTerm)) {
+           temp.push(obj);
+       }
+   })
+   return temp;
+}
+
+module.exports.app = app;
